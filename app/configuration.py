dee8a567fa1f616d3c2f8c98d44736232d3194e9
Mars687p <26.vovchik.26@mail.ru>
2024-02-28 14:16:34 +0300

    new setting

--- app/configuration.py
+++ app/configuration.py
@@ -6,6 +6,9 @@ from app.logs import logger
 def get_url_utm() -> str:
     return config.get('Settings', 'url_utm')
 
+def get_fsrar_id() -> int:
+    return config.getint('Settings', 'fsrar_id')
+
 def get_file_path(name_file) -> str:
     return config.get('Audio', name_file)
 
@@ -15,12 +18,20 @@ def get_bot_token() -> str:
 def get_condition_ships() -> tuple:
     return eval(config.get('Telegram', 'condition_ships'))
 
-def get_conection_database(user) -> str:
+def get_conection_database(user) -> dict:
     return {'db_name': config.get('Database', 'db_name'),
             'host': config.get('Database', 'host'),
             'port': config.get('Database', 'port'),
             'user': user,
             'password': config.get('Users_db', user)}
+
+def get_data_connection_rabbit() -> dict:
+    return {'name_pc': config.get('Rabbit', 'pc'),
+            'user': config.get('Rabbit', 'user'),
+            'password': config.get('Rabbit', 'password')}
+
+def get_start_app() -> bool:
+    return config.getboolean('Settings', 'legacy_start')
 
 try:
     path_file = os.path.realpath(__file__)
