4217f8c539630db23410df273bc0ee9dbba89e16
Mars687p <26.vovchik.26@mail.ru>
2024-07-04 11:52:25 +0300

    refactoring

--- urme/urme/settings.py
+++ urme/urme/settings.py
@@ -12,19 +12,19 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
-from app.configuration import get_conection_database
+from app.configuration import get_conection_database, get_django_key
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = get_django_key()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['']
+ALLOWED_HOSTS = ['127.0.0.1']
 
 # Application definition
 
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'shipments_app',
     'users',
     'reports',
+    'manufactures',
     # 'debug_toolbar',
 ]
 
